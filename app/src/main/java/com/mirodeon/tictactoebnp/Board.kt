a1d45a84e6e698c741ec7b19c8aa25a1b4c1b572
Mirodeon <mehdi.habaru@gmail.com>
2026-01-10 04:42:15 +0100

    feat: expose board size

---
+++ app/src/main/java/com/mirodeon/tictactoebnp/Board.kt
@@ -0,0 +1,5 @@
+package com.mirodeon.tictactoebnp
+
+class Board constructor(
+    val size: Int = 3
+)
--- app/src/main/java/com/mirodeon/tictactoebnp/TicTacToeGame.kt
+++ app/src/main/java/com/mirodeon/tictactoebnp/TicTacToeGame.kt
@@ -2,14 +2,16 @@ package com.mirodeon.tictactoebnp
 
 class TicTacToeGame private constructor(
     val currentPlayer: Player,
-    val status: GameStatus
+    val status: GameStatus,
+    val board: Board
 ) {
 
     companion object {
         fun newGame(): TicTacToeGame {
             return TicTacToeGame(
                 currentPlayer = Player.X,
-                status = GameStatus.InProgress
+                status = GameStatus.InProgress,
+                board = Board()
             )
         }
     }
