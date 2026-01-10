2dec71f5e5e0ace0260dd1009219e6c352e9ec2c
Mirodeon <mehdi.habaru@gmail.com>
2026-01-10 10:14:18 +0100

    feat: detect draw when board is full

--- app/src/main/java/com/mirodeon/tictactoebnp/GameStatus.kt
+++ app/src/main/java/com/mirodeon/tictactoebnp/GameStatus.kt
@@ -2,5 +2,6 @@ package com.mirodeon.tictactoebnp
 
 sealed class GameStatus {
     data object InProgress : GameStatus()
+    data object Draw : GameStatus()
     data class Won(val winner: Player) : GameStatus()
 }
--- app/src/main/java/com/mirodeon/tictactoebnp/Rules.kt
+++ app/src/main/java/com/mirodeon/tictactoebnp/Rules.kt
@@ -8,6 +8,10 @@ object Rules {
         winnerOnMainDiagonal(board)?.let { return GameStatus.Won(it) }
         winnerOnAntiDiagonal(board)?.let { return GameStatus.Won(it) }
 
+        if (isBoardFull(board)) {
+            return GameStatus.Draw
+        }
+
         return GameStatus.InProgress
     }
 
@@ -46,5 +50,16 @@ object Rules {
             }
         }
         return first
+    }
+
+    private fun isBoardFull(board: Board): Boolean {
+        for (row in 0 until board.size) {
+            for (col in 0 until board.size) {
+                if (board.get(Position(row, col)) == null) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 }
