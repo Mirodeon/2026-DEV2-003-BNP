7dc9fc2bfcd74ede8c9547f9627f5d4663fec12c
Mirodeon <mehdi.habaru@gmail.com>
2026-01-10 14:53:38 +0100

    feat: handle cell clicks in viewmodel

--- app/src/main/java/com/mirodeon/tictactoebnp/ui/TicTacToeViewModel.kt
+++ app/src/main/java/com/mirodeon/tictactoebnp/ui/TicTacToeViewModel.kt
@@ -3,8 +3,14 @@ package com.mirodeon.tictactoebnp.ui
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.mirodeon.tictactoebnp.domain.Position
 
 class TicTacToeViewModel : ViewModel() {
     private val _state = mutableStateOf(TicTacToeUiState())
     val state: State<TicTacToeUiState> = _state
+
+    fun onCellClicked(row: Int, col: Int) {
+        val nextGame = _state.value.game.play(Position(row, col))
+        _state.value = _state.value.copy(game = nextGame, errorMessage = null)
+    }
 }
