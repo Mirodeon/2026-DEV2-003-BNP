30b996d16433f6b04582c7e9202791cb6bd5d6c1
Mirodeon <mehdi.habaru@gmail.com>
2026-01-10 14:04:23 +0100

    test: specify viewmodel plays move on cell click

--- app/src/test/java/com/mirodeon/tictactoebnp/ui/TicTacToeViewModelTest.kt
+++ app/src/test/java/com/mirodeon/tictactoebnp/ui/TicTacToeViewModelTest.kt
@@ -2,6 +2,7 @@ package com.mirodeon.tictactoebnp.ui
 
 import com.mirodeon.tictactoebnp.domain.GameStatus
 import com.mirodeon.tictactoebnp.domain.Player
+import com.mirodeon.tictactoebnp.domain.Position
 import org.junit.Assert
 import org.junit.Test
 
@@ -14,6 +15,17 @@ class TicTacToeViewModelTest {
         Assert.assertEquals(Player.X, vm.state.game.currentPlayer)
         Assert.assertEquals(GameStatus.InProgress, vm.state.game.status)
         Assert.assertEquals(3, vm.state.game.board.size)
+        Assert.assertNull(vm.state.errorMessage)
+    }
+
+    @Test
+    fun clicking_empty_cell_plays_a_move() {
+        val vm = TicTacToeViewModel()
+
+        vm.onCellClicked(1, 1)
+
+        Assert.assertEquals(Player.X, vm.state.game.board.get(Position(1, 1)))
+        Assert.assertEquals(Player.O, vm.state.game.currentPlayer)
         Assert.assertNull(vm.state.errorMessage)
     }
 }
