d8daa01d95fd6d197773563662a8c9b72612f5e4
Mirodeon <mehdi.habaru@gmail.com>
2026-01-11 22:39:07 +0100

    test: display X mark when clicking a cell

--- app/src/androidTest/java/com/mirodeon/tictactoebnp/TicTacToeScreenTest.kt
+++ app/src/androidTest/java/com/mirodeon/tictactoebnp/TicTacToeScreenTest.kt
@@ -1,8 +1,11 @@
 package com.mirodeon.tictactoebnp
 
 import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,5 +18,11 @@ class TicTacToeScreenTest {
     fun board_displays_9_cells() {
         rule.onAllNodesWithTag("cell", useUnmergedTree = true)
             .assertCountEquals(9)
+    }
+
+    @Test
+    fun clicking_a_cell_displays_x() {
+        rule.onNodeWithTag("cell-1-1", useUnmergedTree = true).performClick()
+        rule.onNodeWithTag("cell-1-1-text", useUnmergedTree = true).assertTextEquals("X")
     }
 }
